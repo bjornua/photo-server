e9e928d33759041890bfb98a5563d3fd68f5ec33
Bjorn Arnholtz <bjorn.arnholtz@gmail.com>
2021-04-03 00:10:06 +0200

    Forgot a file

---
+++ src/app_state/store/uploads.rs
@@ -0,0 +1,39 @@
+use crate::lib::file;
+use crate::lib::id::Id;
+use std::collections::HashMap;
+
+#[derive(Clone, Debug)]
+pub struct Upload {
+    pub id: Id,
+    pub user_id: Id,
+    pub size: u64,
+    pub type_: file::Type,
+    pub date_uploaded: chrono::DateTime<chrono::Utc>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Uploads {
+    inner: HashMap<Id, Upload>,
+}
+
+impl Uploads {
+    pub fn new() -> Self {
+        Self {
+            inner: HashMap::new(),
+        }
+    }
+
+    pub fn create(&mut self, upload: Upload) {
+        let entry = self.inner.entry(upload.id.clone());
+        match entry {
+            std::collections::hash_map::Entry::Occupied(_) => {
+                panic!("Upload exists")
+            }
+            std::collections::hash_map::Entry::Vacant(e) => e.insert(upload),
+        };
+    }
+
+    pub fn get(&self, session_id: &Id) -> Option<&Upload> {
+        self.inner.get(session_id)
+    }
+}
